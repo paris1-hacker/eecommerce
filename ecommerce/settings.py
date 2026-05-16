cc0d71cd5097e9ebe1c30f4512f04408016e22b0
ofem <ofempatrick10@gmail.com>
2026-05-16 20:04:45 +0100

    updated static files

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles_build')
+STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 
 
 MEDIA_URL = '/media/'
